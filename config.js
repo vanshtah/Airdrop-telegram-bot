babcf850f55cd40f3bef087100351571ce354ee2
vanshtah <34390771+vanshtah@users.noreply.github.com>
2018-09-21 16:02:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // telegram token
-var token = '640674270:AAE6YpEdAJd7Zo-alsFi6SE_JSKlOF7rNDg'
+var token = 'XXXXXXXXXXXXXXXXXXXXXX'
 // firebase config
 var config = {
-    apiKey: "AIzaSyCCIlQLBY34CIp8NS2z_taPKtCSQ9wJs4M",
-    authDomain: "est-bot.firebaseapp.com",
-    databaseURL: "https://est-bot.firebaseio.com/",
-    projectId: "est-bot",
-    storageBucket: "est-bot.appspot.com",
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXX.firebaseapp.com",
+    databaseURL: "https://XXXXXXXXX.firebaseio.com/",
+    projectId: "XXXXXXXX",
+    storageBucket: "XXXXXXXXXXXX.appspot.com",
 };
 
 exports.token = token;
